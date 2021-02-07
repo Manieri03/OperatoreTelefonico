0a982df6e4a5ca7df886cd80aa03914ddf201eaa
Alessio Manieri <mani.alexander03@gmail.com>
2021-02-07 12:48:04 +0100

    Aggiungere i file di progetto.

---
+++ LibreriaTelefono/Contatto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LibreriaTelefono
+{
+    public class Contatto
+    {
+        public string Nome { get; private set; }
+        public string Cognome { get; private set; }
+        public long NTel { get; private set; }
+
+        public Contatto(string Nome, string Cognome, long NTel)
+        {
+            this.Nome = Nome;
+            this.Cognome = Cognome;
+            if (NTel.ToString().Length != 10)
+                throw new Exception("Il numero deve contenere esattamente 10 cifre");
+            else
+                this.NTel = NTel;
+        }
+    }
+}
